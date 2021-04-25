67a231ed3e41cbeb6b1647e60ee7da2a5212a03e
Diztenz <cosmoc05@yahoo.com>
2021-04-25 16:21:10 -0700

    added correct elapsed expected time

--- prompt.js
+++ prompt.js
@@ -145,6 +145,6 @@ setTimeout(() => {
   const millis = Date.now() - start;
 
   console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
-  // expected output: seconds elapsed = 2
+  // expected output: seconds elapsed = 10 for 10000/1000 = 10
 }, 10000);
 */
