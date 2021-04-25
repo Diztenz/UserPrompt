65a30bd2c520411a1a77c501c3f3719e86ea32da
Diztenz <cosmoc05@yahoo.com>
2021-04-25 16:17:20 -0700

    added sample date.now() timer again

--- prompt.js
+++ prompt.js
@@ -133,3 +133,18 @@ var timeLeft = parseInt(secondsPerExercise);
           .innerHTML = array2[i++];    // get the item and increment
       if (i == array2.length) i = 0;   // reset to first element if you've reached the end
   }, parseInt(secondsPerExercise * 1000));
+
+  /*
+  // this example takes 2 seconds to run
+const start = Date.now();
+
+console.log('starting timer...');
+// expected output: starting timer...
+
+setTimeout(() => {
+  const millis = Date.now() - start;
+
+  console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
+  // expected output: seconds elapsed = 2
+}, 10000);
+*/
